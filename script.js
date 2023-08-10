33406821688d8a38df7dce542648360c73f5320b
Shaik8327 <138495263+Shaik8327@users.noreply.github.com>
2023-08-10 21:31:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".typing",{
+    Strings:["","Web Designer""Student",],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
+
+const nav = document.querySelector('.nav'),
+navList = nav.querySelectorAll('li'),
+totalNavList = navList.length;
+for(let i=0; i<totalNavList; i++)
+{
+    document.write(navList[i])
+}
